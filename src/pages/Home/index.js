ec179b26df98ff330aacca2da841971f82d22eb2
David Rocha <davidrockkj@gmail.com>
2024-03-14 18:47:53 -0300

    Puxando dados da API do github // criando gerenciamento de destado para o usuário e o repositório // criando condição ternária para mostrar informações do usuário na tela // criando condição ternária para mostrar informações dos repositórios na tela

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,6 +9,25 @@ import ItemList from '../../components/ItemList';
 function App() {
 
   const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if (newUser.name){
+      const {avatar_url, name, bio} = newUser;
+      setCurrentUser({avatar_url, name, bio});
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+
+      if (newRepos.lenght){
+        setRepos(newRepos)
+      }
+    }
+  }
 
   return (
     <div className="App">
@@ -29,26 +48,36 @@ function App() {
               value={user}
               onChange={event => setUser(event.target.value)}
             />
-            <button>Buscar</button>
+            <button onClick={handleGetData}>Buscar</button>
           </div>
 
-          <div className='perfil'>
-            <img src="https://avatars.githubusercontent.com/u/62350916?s=96&v=4" className='profile' alt='User pic' />
+          {currentUser?.name ? (
+            <>
+              <div className='perfil'>
+                <img src="https://avatars.githubusercontent.com/u/62350916?s=96&v=4" className='profile' alt='User pic' />
 
-            <div>
-              <h3>David Rocha</h3>
-              <span>@davidrockkj</span>
-              <p>Descrição</p>
-            </div>
+                <div>
+                  <h3>David Rocha</h3>
+                  <span>@davidrockkj</span>
+                  <p>Descrição</p>
+                </div> 
 
-          </div> {/* perfil */}
-          <hr />
-          <div>
-            <h4 className='repositorio'>Repositórios</h4>
-            <ItemList
-              title='Teste1'
-              description='tesde de descrição' />
-          </div> {/* repositorios */}
+              <hr />
+              </div> {/* perfil */}
+            </>
+          ) : null}
+
+          {repos?.lenght ? (
+            <>
+              <div>
+                <h4 className='repositorio'>Repositórios</h4>
+                <ItemList
+                  title='Teste1'
+                  description='tesde de descrição' />
+              </div> {/* repositorios */}
+            </>
+          ) : null}
+          
         </div> {/* info */}
       </div> {/* conteudo */}
     </div>
