5be1c290ce430d59fe0ac3433edd16a6e2532c46
David Rocha <davidrockkj@gmail.com>
2024-03-14 17:32:07 -0300

    Adicionando imagem, nome e descrição

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,14 +10,26 @@ function App() {
     <div className="App">
       <Header />
       <div className='conteudo'>
+
         <img
             src={background}
             alt='background'
             className='background'
           />
+
         <div className='info'>
           <input name='usuario' placeholder='@username' />
           <button>Buscar</button>
+        </div>
+
+        <div className='perfil'>
+          <img src="https://avatars.githubusercontent.com/u/62350916?s=96&v=4" className='profile' alt='User pic' />
+
+          <div>
+            <h3>David Rocha</h3>
+            <p>Descrição</p>
+
+          </div>
         </div>
       </div>
     </div>
