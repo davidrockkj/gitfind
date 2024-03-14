d26016182d91c5d09a31f2cdda1132b3e0b3de3e
David Rocha <davidrockkj@gmail.com>
2024-03-14 17:02:13 -0300

    Colocando o import da imagem no arquivo correto e posicionando na tela

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,10 @@
 import './styles.css';
-import background from '../../assets/background.png';
 
 const Header = () => {
   return (
     <header>
       <h1>GitFind</h1>
       <div className='conteudo'>
-        <img
-          src={background}
-          alt='background'
-          className='background'
-        />
       </div>
     </header>
   )
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,19 @@
 import { Header } from '../../components/Header';
+import './styles.css';
+import background from '../../assets/background.png';
+
+
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <img
+          src={background}
+          alt='background'
+          className='background'
+        />
     </div>
   );
 }
