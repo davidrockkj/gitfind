77564b20bd68e21dde8604207a8553ade68c2cd2
David Rocha <davidrockkj@gmail.com>
2024-03-14 16:54:38 -0300

    Implementando a imagem de background

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,17 @@
 import './styles.css';
+import background from '../../assets/background.png';
 
 const Header = () => {
   return (
     <header>
       <h1>GitFind</h1>
+      <div className='conteudo'>
+        <img
+          src={background}
+          alt='background'
+          className='background'
+        />
+      </div>
     </header>
   )
 }
